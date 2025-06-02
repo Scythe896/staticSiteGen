3b259dd8c4f74e9356c4f36d4f1ab45e53f07300
scythe896 <scythe096@gmail.com>
2025-06-02 13:31:32 +1000

    Ch5 L4

--- src/generatePage.py
+++ src/generatePage.py
@@ -1,6 +1,27 @@
 import os
-from htmlnode import HTMLNode
+import shutil
 from markdown import markdown_to_blocks, markdown_to_html_node
+
+def static_to_public(src, destination):
+    print(f"Looking for {destination}")
+    if not os.path.exists(destination):
+        print("Not Found making directory")
+        os.mkdir(destination)
+    else:
+        print("Found removing files")
+        shutil.rmtree(destination)
+        os.mkdir(destination)
+    src_list = os.listdir(src)
+    for item in src_list:
+        item_path = f"{src}/{item}"
+        destination_path = f"{destination}/{item}"
+        print(f"Examining: {item_path}")
+        if os.path.isfile(item_path):
+            print(f"Copying file: {item_path} to {destination_path}")
+            shutil.copy(item_path, destination_path)
+        else:
+            print(f"Calling function on {item_path} and {destination_path}")
+            static_to_public(item_path, destination_path)
 
 def extract_title(markdown):
     blocks = markdown_to_blocks(markdown)
@@ -24,4 +45,13 @@ def generate_page(from_path, template_path, destination_path):
     with open(destination_path, "w") as f:
         f.write(template_contents)
 
-
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    content_list = os.listdir(dir_path_content)
+    for item in content_list:
+        item_path = os.path.join(dir_path_content, item)
+        dest_path = os.path.join(dest_dir_path, item)
+        if os.path.isfile(item_path) and item.split(".")[1] == "md":
+            generate_page(item_path, template_path, os.path.join(dest_dir_path, "index.html"))
+        else:
+            os.mkdir(dest_path)
+            generate_pages_recursive(item_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,34 +1,8 @@
-import os
-import shutil
-from textnode import TextNode
-from generatePage import generate_page
-
-def static_to_public(src, destination):
-    print(f"Looking for {destination}")
-    if not os.path.exists(destination):
-        print("Not Found making directory")
-        os.mkdir(destination)
-    else:
-        print("Found removing files")
-        shutil.rmtree(destination)
-        os.mkdir(destination)
-    src_list = os.listdir(src)
-    for item in src_list:
-        item_path = f"{src}/{item}"
-        destination_path = f"{destination}/{item}"
-        print(f"Examining: {item_path}")
-        if os.path.isfile(item_path):
-            print(f"Copying file: {item_path} to {destination_path}")
-            shutil.copy(item_path, destination_path)
-        else:
-            print(f"Calling function on {item_path} and {destination_path}")
-            static_to_public(item_path, destination_path)
-    
-
+from generatePage import generate_pages_recursive, static_to_public
 
 def main():
     static_to_public("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 main()
 
