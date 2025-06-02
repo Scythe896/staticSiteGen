0d2e3219c5b8c493da7f97191ae62eb721f13b54
scythe896 <scythe096@gmail.com>
2025-06-02 14:32:08 +1000

    Ch5: L5

--- src/generatePage.py
+++ src/generatePage.py
@@ -30,7 +30,7 @@ def extract_title(markdown):
             return block.strip("# ")
     raise ValueError("No h1 heading")
 
-def generate_page(from_path, template_path, destination_path):
+def generate_page(from_path, template_path, destination_path, base_path):
     print(f"Generating page from {from_path} to {destination_path} using {template_path}")
     with open(from_path) as f:
         markdown_contents = f.read()
@@ -40,18 +40,19 @@ def generate_page(from_path, template_path, destination_path):
     html = node.to_html()
     title = extract_title(markdown_contents)
     template_contents = template_contents.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    template_contents = template_contents.replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}')
     if not os.path.exists(destination_path.split("/")[0]):
         os.makedirs(destination_path.split("/")[0])
     with open(destination_path, "w") as f:
         f.write(template_contents)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     content_list = os.listdir(dir_path_content)
     for item in content_list:
         item_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(item_path) and item.split(".")[1] == "md":
-            generate_page(item_path, template_path, os.path.join(dest_dir_path, "index.html"))
+            generate_page(item_path, template_path, os.path.join(dest_dir_path, "index.html"), base_path)
         else:
             os.mkdir(dest_path)
-            generate_pages_recursive(item_path, template_path, dest_path)
+            generate_pages_recursive(item_path, template_path, dest_path, base_path)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
+import sys
+import os
 from generatePage import generate_pages_recursive, static_to_public
 
+base_path = "/"
+if sys.argv[0] != "":
+    base_path = sys.argv[0]
+
 def main():
-    static_to_public("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    static_to_public("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", base_path)
 
 main()
 
